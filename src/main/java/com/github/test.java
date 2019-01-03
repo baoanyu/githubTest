9cc37be1865dce09d11828c1b5ad80785a6d8fb4
baoanyu <554287113@qq.comm>
2019-01-03 14:16:34 +0800

    gitHub测试 添加for循环

--- src/main/java/com/github/test.java
+++ src/main/java/com/github/test.java
@@ -2,7 +2,11 @@ package com.github;
 
 public class test {
     public static void main(String[] args) {
-        String a="\u4f18\u79c0";
-        System.out.println(a);
+        for (int i = 0; i < 10; i++) {
+            String a="\u4f18\u79c0";
+            System.out.println(a);
+        }
+        System.out.println("叼叼叼！");
     }
+
 }
